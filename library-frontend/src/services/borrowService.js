310dc6a559d6b8d1048813c680f2e557af9e96c7
aman0217 <ithakur021751@gmail.com>
2026-08-08 18:52:02 +0530

    fix borrow API endpoints

--- library-frontend/src/services/borrowService.js
+++ library-frontend/src/services/borrowService.js
@@ -1,26 +1,27 @@
 import api from "../api/axiosConfig";
 
+import api from "../api/axiosConfig";
 export const issueBook = async (data) => {
-    const response = await api.post("/borrow", data);
+    const response = await api.post("/issues", data);
     return response.data;
 };
 
 export const returnBook = async (issueId) => {
-    const response = await api.put(`/borrow/return/${issueId}`);
+    const response = await api.put(`/issues/${issueId}/return`);
     return response.data;
 };
 
 export const getBorrowHistory = async (userId) => {
-    const response = await api.get(`/borrow/history/${userId}`);
+    const response = await api.get(`/issues/student/${userId}`);
     return response.data;
 };
 
 export const getCurrentBorrowedBooks = async (userId) => {
-    const response = await api.get(`/borrow/current/${userId}`);
+    const response = await api.get(`/issues/student/${userId}/current`);
     return response.data;
 };
 
 export const getOverdueBooks = async () => {
-    const response = await api.get("/borrow/overdue");
+    const response = await api.get("/issues/overdue");
     return response.data;
 };
